d670f624ae9af7b24ebf635f029afcc0b98f1822
Skintrphoenix <randy.phoenix.098@gmail.com>
2023-02-11 10:25:20 +0700

    testing

--- src/PluginLoader.php
+++ src/PluginLoader.php
@@ -4,6 +4,17 @@ namespace Skintrphoenix\PluginLoader;
 
 class PluginLoader{
 
-    private function __construct(){}
-    
+    private $base_folder;
+
+    private function __construct(){
+        $this->base_folder = base_path('plugins');
+        if(!is_dir($this->base_folder)){
+            @mkdir($this->base_folder);
+        }
+    }
+
+    public function getAllPlugins():array{
+        return scandir($this->base_folder);
+    }
+
 }
