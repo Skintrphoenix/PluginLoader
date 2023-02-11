697abce02ddfc6fe0864473f263e338f0f0f0eb4
Skintrphoenix <randy.phoenix.098@gmail.com>
2023-02-11 11:05:06 +0700

    update get list plugin

--- src/PluginLoader.php
+++ src/PluginLoader.php
@@ -7,14 +7,27 @@ class PluginLoader{
     private $base_folder;
 
     public function __construct(){
-        $this->base_folder = base_path('plugins');
+        $this->base_folder = base_path('plugins') . '/';
         if(!is_dir($this->base_folder)){
             @mkdir($this->base_folder);
         }
     }
 
     public function getAllPlugins():array{
-        return scandir($this->base_folder);
+        $plugin = [];
+
+        $data = scandir($this->base_folder);
+        foreach($data as $item){
+            $file = $this->base_folder . $item . '/plugin.json';
+            if(is_file($file)){
+                $json = json_decode(file_get_contents($file));
+                if(isset($json->name)){
+                    $plugin[] = $json->name;
+                }
+            }
+        }
+
+        return $plugin;
     }
 
 }
