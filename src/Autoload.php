a3166c7fd050a9667c374240c272cd934329c39f
Skintrphoenix <randy.phoenix.098@gmail.com>
2023-02-16 15:24:24 +0700

    check function exists

--- src/Autoload.php
+++ src/Autoload.php
@@ -1,25 +1,30 @@
 <?php
 
-function rsearch($folder, $regPattern) {
-    $dir = new RecursiveDirectoryIterator($folder);
-    $ite = new RecursiveIteratorIterator($dir);
-    $files = new RegexIterator($ite, $regPattern, RegexIterator::GET_MATCH);
-    $fileList = array();
-    foreach($files as $file) {
-        $fileList = array_merge($fileList, $file);
+if (! function_exists('rsearch')) {
+    function rsearch($folder, $regPattern) {
+        $dir = new RecursiveDirectoryIterator($folder);
+        $ite = new RecursiveIteratorIterator($dir);
+        $files = new RegexIterator($ite, $regPattern, RegexIterator::GET_MATCH);
+        $fileList = array();
+        foreach($files as $file) {
+            $fileList = array_merge($fileList, $file);
+        }
+        return $fileList;
     }
-    return $fileList;
 }
 
-function load($path){
-    $result = rsearch($path, '/.*\/*\.php/');
-    foreach($result as $file){
-        $tokens = token_get_all(file_get_contents($file));
-        foreach($tokens as $data){
-            if(is_array($data)){
-                if($data[1] == 'class'){
-                    if(!class_exists($file)){
-                        require $file;
+if (! function_exists('load')) {
+    function load($path)
+    {
+        $result = rsearch($path, '/.*\/*\.php/');
+        foreach ($result as $file) {
+            $tokens = token_get_all(file_get_contents($file));
+            foreach ($tokens as $data) {
+                if (is_array($data)) {
+                    if ($data[1] == 'class') {
+                        if (!class_exists($file)) {
+                            require $file;
+                        }
                     }
                 }
             }
--- src/function.php
+++ src/function.php
@@ -1,33 +1,29 @@
 <?php
 
+if (! function_exists('custom_copy')) {
+    function custom_copy($src, $dst)
+    {
+        // open the source directory
+        $dir = opendir($src);
 
-function custom_copy($src, $dst) {
+        // Make the destination directory if not exist
+        @mkdir($dst);
 
-	// open the source directory
-	$dir = opendir($src);
-
-	// Make the destination directory if not exist
-	@mkdir($dst);
-
-	// Loop through the files in source directory
-	while( $file = readdir($dir) ) {
-
-		if (( $file != '.' ) && ( $file != '..' )) {
-			if ( is_dir($src . '/' . $file) )
-			{
-
-				// Recursively calling custom copy function
-				// for sub directory
-				custom_copy($src . '/' . $file, $dst . '/' . $file);
-
-			}
-			else {
-                if(!file_exists($dst . '/' . $file)){
-                    copy($src . '/' . $file, $dst . '/' . $file);
+        // Loop through the files in source directory
+        while ($file = readdir($dir)) {
+            if (($file != '.') && ($file != '..')) {
+                if (is_dir($src . '/' . $file)) {
+                    // Recursively calling custom copy function
+                    // for sub directory
+                    custom_copy($src . '/' . $file, $dst . '/' . $file);
+                } else {
+                    if (!file_exists($dst . '/' . $file)) {
+                        copy($src . '/' . $file, $dst . '/' . $file);
+                    }
                 }
-			}
-		}
-	}
+            }
+        }
 
-	closedir($dir);
+        closedir($dir);
+    }
 }
