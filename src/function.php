ecbcd653b0f1bbb5bebeef867c1fd83da0cc5620
Skintrphoenix <randy.phoenix.098@gmail.com>
2023-02-16 14:32:55 +0700

    update data_folder

---
+++ src/function.php
@@ -0,0 +1,33 @@
+<?php
+
+
+function custom_copy($src, $dst) {
+
+	// open the source directory
+	$dir = opendir($src);
+
+	// Make the destination directory if not exist
+	@mkdir($dst);
+
+	// Loop through the files in source directory
+	while( $file = readdir($dir) ) {
+
+		if (( $file != '.' ) && ( $file != '..' )) {
+			if ( is_dir($src . '/' . $file) )
+			{
+
+				// Recursively calling custom copy function
+				// for sub directory
+				custom_copy($src . '/' . $file, $dst . '/' . $file);
+
+			}
+			else {
+                if(!file_exists($dst . '/' . $file)){
+                    copy($src . '/' . $file, $dst . '/' . $file);
+                }
+			}
+		}
+	}
+
+	closedir($dir);
+}
